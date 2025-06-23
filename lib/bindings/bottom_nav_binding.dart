fbf7ee698275698bdf7fefa8ecc476a1a5c3fc0d
Balbino Pedro Baptista <dinubatista4@gmail.com>
2025-06-24 00:14:41 +0700

    feat: add bottomnav binding

---
+++ lib/bindings/bottom_nav_binding.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:higrow/controllers/bottom_nav_controller.dart';
+import 'package:higrow/controllers/dashboard_controller.dart';
+import 'package:higrow/controllers/history_controller.dart';
+
+class BottomNavBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => BottomNavController());
+    Get.lazyPut(() => DashboardController());
+    Get.lazyPut(() => HistoryController());
+  }
+}
