e0c2f7e7fa14c1434d97a8a6dc57e248852aca26
Balbino Pedro Baptista <dinubatista4@gmail.com>
2025-06-30 14:14:02 +0700

    feat: add camera binding

---
+++ lib/bindings/cam_binding.dart
@@ -0,0 +1,11 @@
+
+import 'package:get/get.dart';
+import 'package:higrow/controllers/cam_controller.dart';
+
+class CamBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(()=>CamController());
+  }
+  
+}
