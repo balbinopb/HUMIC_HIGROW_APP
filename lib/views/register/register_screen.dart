f15e069d761d1e57210d1bddf33acac2058a3879
Balbino Pedro Baptista <dinubatista4@gmail.com>
2025-07-18 00:24:23 +0700

    feat(register): update register screen

--- lib/views/register/register_screen.dart
+++ lib/views/register/register_screen.dart
@@ -15,111 +15,118 @@ class RegisterScreen extends GetView<AuthController> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.all(35),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // LOGO
-                SizedBox(
-                  height: 34,
-                  width: 102,
-                  child: Image.asset("assets/images/logo.png"),
-                ),
-                SizedBox(height: 60),
-          
-                // TITLE
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Create New Account",
-                    style: GoogleFonts.inter(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.black,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-          
-                // USERNAME
-                InputField(
-                  hintText: 'Username',
-                  controller: controller.usernameController,
-                ),
-                SizedBox(height: 16),
-          
-                // EMAIL
-                InputField(
-                  hintText: 'Email',
-                  controller: controller.emailController,
-                ),
-                SizedBox(height: 16),
-          
-                // PASSWORD
-                Obx(
-                  () => InputField(
-                    hintText: 'Password',
-                    obscureText: controller.isObscure.value,
-                    controller: controller.passwordController,
-                  ),
-                ),
-                SizedBox(height: 50),
-          
-                // BUTTON
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print("========================================BEFORE REGISTER=================================================");
-                      controller.register();
-                      print("========================================AFTER REGISTER=================================================");
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.orange,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(7),
-                      ),
-                    ),
-                    child: Text(
-                      "Create Account",
-                      style: GoogleFonts.inter(
-                        color: AppColors.white,
-                        fontSize: 13,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 35),
-          
-                // HAVE AN ACOUNT?LOG IN
-                Row(
+            padding: EdgeInsets.symmetric(horizontal: 35),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight:
+                    MediaQuery.of(context).size.height -
+                    MediaQuery.of(context).padding.top,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text(
-                      "Have an account?",
-                      style: GoogleFonts.inter(
-                        color: Colors.grey.shade400,
-                        fontSize: 13,
-                        fontWeight: FontWeight.w600,
-                      ),
+                    // LOGO
+                    SizedBox(
+                      height: 34,
+                      width: 102,
+                      child: Image.asset("assets/images/logo.png"),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        Get.toNamed(AppRoutes.login);
-                      },
+                    SizedBox(height: 60),
+
+                    // TITLE
+                    Align(
+                      alignment: Alignment.centerLeft,
                       child: Text(
-                        "Log In",
+                        "Create New Account",
                         style: GoogleFonts.inter(
-                          color: AppColors.orange,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.black,
                         ),
                       ),
                     ),
+                    SizedBox(height: 20),
+
+                    // EMAIL
+                    InputField(
+                      hintText: 'Email',
+                      controller: controller.emailController,
+                    ),
+                    SizedBox(height: 16),
+
+                    // PASSWORD
+                    Obx(
+                      () => InputField(
+                        hintText: 'Password',
+                        obscureText: controller.isObscure.value,
+                        controller: controller.passwordController,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    // BUTTON
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print(
+                            "[DEBUG]========================================BEFORE REGISTER=================================================",
+                          );
+                          controller.register();
+                          print(
+                            "[DEBUG]========================================AFTER REGISTER=================================================",
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.orange,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                        ),
+                        child: Text(
+                          "Create Account",
+                          style: GoogleFonts.inter(
+                            color: AppColors.white,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+
+                    // HAVE AN ACOUNT?LOG IN
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Have an account?",
+                          style: GoogleFonts.inter(
+                            color: Colors.grey.shade400,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Get.toNamed(AppRoutes.login);
+                          },
+                          child: Text(
+                            "Log In",
+                            style: GoogleFonts.inter(
+                              color: AppColors.orange,
+                              fontSize: 13,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
